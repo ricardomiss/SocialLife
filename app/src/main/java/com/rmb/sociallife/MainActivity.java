8703d5772dd7ab1af904104773ed8e8d63552348
JesusRosml <jesusperezhidalgo50@gmail.com>
2023-04-30 14:33:44 -0600

    Toolbar modificado, agregue una imagen y cambie el fondo a transparente

--- app/src/main/java/com/rmb/sociallife/MainActivity.java
+++ app/src/main/java/com/rmb/sociallife/MainActivity.java
@@ -3,6 +3,7 @@ package com.rmb.sociallife;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
